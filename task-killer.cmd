5c7cbeaf21f3bfec2c6ab07a442e7037d4f6284d
supmaxi <samrambo117@gmail.com>
2020-04-07 18:55:24 +1000

    Update task-killer.cmd

--- task-killer.cmd
+++ task-killer.cmd
@@ -1,1 +1,2 @@
+# Replace 'kodi' with any app name. For example 'chrome' or 'firefox'
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "Get-Process|Where-Object {$_.Name -like 'kodi'}|Stop-Process"
